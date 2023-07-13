51470beab3c0099d3596157396f65f7fa16a7f14
Luna <imlvnaa@gmail.com>
2023-07-13 17:51:04 -0400

    [sayas]: finish command

--- src/commands/sayas.ts
+++ src/commands/sayas.ts
@@ -1,6 +1,11 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ChannelType,
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+} from "discord.js";
 
 import { SlashCommand } from "../commands";
+import bot from "..";
 
 const command: SlashCommand = {
   data: new SlashCommandBuilder()
@@ -19,9 +24,51 @@ const command: SlashCommand = {
         .setRequired(true)
     ),
   callback: async (interaction: ChatInputCommandInteraction) => {
-    interaction.reply({
-      content: "a",
-      ephemeral: true,
+    const target = interaction.options.getUser("user", true);
+    const message = interaction.options.getString("message", true);
+
+    if (!interaction.guild?.members.me?.permissions.has("ManageWebhooks"))
+      return interaction.reply({
+        content: "Bot does not have permission",
+        ephemeral: true,
+      });
+
+    if (!interaction.channel)
+      return interaction.reply({
+        content: "Channel not found",
+        ephemeral: true,
+      });
+
+    let channel = interaction.channel;
+
+    if (channel.partial) channel = await channel.fetch();
+    if (channel.type !== ChannelType.GuildText)
+      return interaction.reply({
+        content: "Channel is not a text channel",
+        ephemeral: true,
+      });
+
+    await interaction.deferReply({ ephemeral: true });
+
+    let webhook = (await channel.fetchWebhooks()).find(
+      (webhook) => webhook.name === `${bot.user!.username}_sayas`
+    );
+
+    if (!webhook) {
+      webhook = await channel.createWebhook({
+        name: `${bot.user!.username}_sayas`,
+        avatar: bot.user!.displayAvatarURL(),
+      });
+    }
+
+    await webhook.send({
+      content: message,
+      username: target.username,
+      avatarURL: target.displayAvatarURL(),
+    });
+
+    await interaction.followUp({
+      content: `Done!`,
     });
   },
 };
