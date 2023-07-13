2c8d1ea5c22eb803177be80a81840dc410561bcb
Luna <imlvnaa@gmail.com>
2023-07-13 18:27:30 -0400

    add purge command

---
+++ src/commands/purge.ts
@@ -0,0 +1,46 @@
+import {
+  ChannelType,
+  ChatInputCommandInteraction,
+  GuildMember,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+} from "discord.js";
+
+import { SlashCommand } from "../commands";
+import bot from "..";
+
+const command: SlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName("purge")
+    .setDescription("Purge messages")
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("Amount of messages to purge")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+  callback: async (interaction: ChatInputCommandInteraction) => {
+    const amount = interaction.options.getInteger("amount", true);
+    const messages = await interaction.channel?.messages.fetch({
+      limit: amount,
+    });
+    if (!messages)
+      return interaction.reply({
+        content: "Could not fetch messages",
+        ephemeral: true,
+      });
+    if (messages.size <= 1)
+      return interaction.reply({
+        content: "Could not purge messages",
+        ephemeral: true,
+      });
+    messages.forEach(async (message) => await message.delete());
+    interaction.reply({
+      content: `Purged ${messages.size} messages`,
+      ephemeral: true,
+    });
+  },
+};
+
+export default command;
