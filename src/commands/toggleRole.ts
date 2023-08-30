69f52629be23a9d5be03254144dde26ce36c57b5
Luna <imlvnaa@gmail.com>
2023-08-29 21:44:53 -0400

    Add a toggle role command

---
+++ src/commands/toggleRole.ts
@@ -0,0 +1,40 @@
+import {
+  ChatInputCommandInteraction,
+  GuildMemberRoleManager,
+  PermissionsBitField,
+  SlashCommandBuilder,
+} from "discord.js";
+import { SlashCommand } from "../commands";
+
+const command: SlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName("toggleRole")
+    .setDescription("Toggle a role on yourself")
+    .addRoleOption((option) =>
+      option.setName("role").setDescription("The role").setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageRoles),
+  callback: async (interaction: ChatInputCommandInteraction) => {
+    const role = interaction.options.getRole("role", true);
+
+    if (
+      (interaction.member!.roles as GuildMemberRoleManager).cache.has(role.id)
+    ) {
+      await (interaction.member!.roles as GuildMemberRoleManager).remove(
+        role.id
+      );
+      return interaction.reply({
+        content: `Removed role ${role.name}`,
+        ephemeral: true,
+      });
+    } else {
+      await (interaction.member!.roles as GuildMemberRoleManager).add(role.id);
+      return interaction.reply({
+        content: `Added role ${role.name}`,
+        ephemeral: true,
+      });
+    }
+  },
+};
+
+export default command;
